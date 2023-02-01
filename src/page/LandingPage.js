f882ef24f2e7be7315d435a9f85b2abd9668d153
iqbalaulia <iqbal.developer16@gmail.com>
2023-02-01 14:20:20 +0700

    [update] testimonial

--- src/page/LandingPage.js
+++ src/page/LandingPage.js
@@ -21,7 +21,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
-        <Testimonial />
+        <Testimonial data={landingPage.testimonial} />
       </>
     );
   }
