327d9649796e03d8c8b65b4cd4d865f5e1bcfe1d
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-02-03 17:15:04 +0700

    feat(details): page details finish

--- src/page/DetailsPage.js
+++ src/page/DetailsPage.js
@@ -2,50 +2,63 @@
 
 import React, { Component } from "react";
 
+import Fade from "react-reveal/Fade";
+
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImages from "parts/FeaturedImages";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimonial from "parts/Testimonial";
+import Footer from "parts/Footer";
 
 import ItemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
-	componentDidMount() {
-		window.title = "Details Page";
-		window.scrollTo(0, 0);
-	}
+  componentDidMount() {
+    window.title = "Details Page";
+    window.scrollTo(0, 0);
+  }
 
-	render() {
-		const breadcrumbList = [
-			{
-				pageTitle: "Home",
-				pageHref: "",
-			},
-			{
-				pageTitle: "Details Page",
-				pageHref: "",
-			},
-		];
+  render() {
+    const breadcrumbList = [
+      {
+        pageTitle: "Home",
+        pageHref: "",
+      },
+      {
+        pageTitle: "Details Page",
+        pageHref: "",
+      },
+    ];
 
-		return (
-			<>
-				<Header {...this.props}></Header>
-				<PageDetailTitle
-					data={ItemDetails}
-					breadcrumb={breadcrumbList}></PageDetailTitle>
-				<FeaturedImages data={ItemDetails.imageUrls} />
-				<section className="container">
-					<div className="row">
-						<div className="col-7 pr-5">
-							<PageDetailDescription data={ItemDetails} />
-						</div>
-						<div className="col-5">
-							<BookingForm itemDetails={ItemDetails}/>
-						</div>
-					</div>
-				</section>
-			</>
-		);
-	}
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <PageDetailTitle
+          data={ItemDetails}
+          breadcrumb={breadcrumbList}
+        ></PageDetailTitle>
+        <FeaturedImages data={ItemDetails.imageUrls} />
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
+            </div>
+            <div className="col-5">
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
+            </div>
+          </div>
+        </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimonial data={ItemDetails.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
